9e95842667e0046b6373c8ad604e0787b556cace
Piotr Biegaj <PeteTheSadPanda@users.noreply.github.com>
2021-01-16 11:00:40 -0600

    Update loading of decorators to the more modern approach of using SolidusSupport::EngineExtension

--- lib/spree_gateway/engine.rb
+++ lib/spree_gateway/engine.rb
@@ -1,32 +1,35 @@
 module SpreeGateway
   class Engine < Rails::Engine
+    include SolidusSupport::EngineExtensions
+
+    isolate_namespace Spree
     engine_name 'solidus_gateway'
 
     initializer "spree.gateway.payment_methods", :after => "spree.register.payment_methods" do |app|
-        app.config.spree.payment_methods << Spree::Gateway::AuthorizeNetCim
-        app.config.spree.payment_methods << Spree::Gateway::AuthorizeNet
-        app.config.spree.payment_methods << Spree::Gateway::CardSave
-        app.config.spree.payment_methods << Spree::Gateway::Eway
-        app.config.spree.payment_methods << Spree::Gateway::Linkpoint
-        app.config.spree.payment_methods << Spree::Gateway::Moneris
-        app.config.spree.payment_methods << Spree::Gateway::PayJunction
-        app.config.spree.payment_methods << Spree::Gateway::PayPalGateway
-        app.config.spree.payment_methods << Spree::Gateway::SagePay
-        app.config.spree.payment_methods << Spree::Gateway::Beanstream
-        app.config.spree.payment_methods << Spree::Gateway::BraintreeGateway
-        app.config.spree.payment_methods << Spree::Gateway::StripeGateway
-        app.config.spree.payment_methods << Spree::Gateway::Worldpay
-        app.config.spree.payment_methods << Spree::Gateway::Banwire
-        app.config.spree.payment_methods << Spree::Gateway::UsaEpay
-        app.config.spree.payment_methods << Spree::Gateway::BalancedGateway
-        app.config.spree.payment_methods << Spree::Gateway::DataCash
-        app.config.spree.payment_methods << Spree::Gateway::PinGateway
-        app.config.spree.payment_methods << Spree::Gateway::Paymill
-        app.config.spree.payment_methods << Spree::Gateway::PayflowPro
-        app.config.spree.payment_methods << Spree::Gateway::SecurePayAu
-        app.config.spree.payment_methods << Spree::Gateway::Maxipago
-        app.config.spree.payment_methods << Spree::Gateway::Migs
-        app.config.spree.payment_methods << Spree::Gateway::SpreedlyCoreGateway
+        app.config.spree.payment_methods << "Spree::Gateway::AuthorizeNetCim"
+        app.config.spree.payment_methods << "Spree::Gateway::AuthorizeNet"
+        app.config.spree.payment_methods << "Spree::Gateway::CardSave"
+        app.config.spree.payment_methods << "Spree::Gateway::Eway"
+        app.config.spree.payment_methods << "Spree::Gateway::Linkpoint"
+        app.config.spree.payment_methods << "Spree::Gateway::Moneris"
+        app.config.spree.payment_methods << "Spree::Gateway::PayJunction"
+        app.config.spree.payment_methods << "Spree::Gateway::PayPalGateway"
+        app.config.spree.payment_methods << "Spree::Gateway::SagePay"
+        app.config.spree.payment_methods << "Spree::Gateway::Beanstream"
+        app.config.spree.payment_methods << "Spree::Gateway::BraintreeGateway"
+        app.config.spree.payment_methods << "Spree::Gateway::StripeGateway"
+        app.config.spree.payment_methods << "Spree::Gateway::Worldpay"
+        app.config.spree.payment_methods << "Spree::Gateway::Banwire"
+        app.config.spree.payment_methods << "Spree::Gateway::UsaEpay"
+        app.config.spree.payment_methods << "Spree::Gateway::BalancedGateway"
+        app.config.spree.payment_methods << "Spree::Gateway::DataCash"
+        app.config.spree.payment_methods << "Spree::Gateway::PinGateway"
+        app.config.spree.payment_methods << "Spree::Gateway::Paymill"
+        app.config.spree.payment_methods << "Spree::Gateway::PayflowPro"
+        app.config.spree.payment_methods << "Spree::Gateway::SecurePayAu"
+        app.config.spree.payment_methods << "Spree::Gateway::Maxipago"
+        app.config.spree.payment_methods << "Spree::Gateway::Migs"
+        app.config.spree.payment_methods << "Spree::Gateway::SpreedlyCoreGateway"
     end
 
     # The application_id is a class attribute on all gateways and is used to
